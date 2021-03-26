88af07469638dc50c7bdbdb626695824c059cc3f
amit <[amirana14325@gmail.com]>
2021-03-27 00:12:07 +0530

    [Amit Rana]Added::adding contact

---
+++ AddressBookDemo/AddressBookDemo/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace AddressBookDemo
+{
+    public class Contact
+    {  
+        
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+                public string Address { get; set; }
+                public string City { get; set; }
+                public string State { get; set; }
+                public string Email { get; set; }
+                public int Zip { get; set; }
+                public long PhoneNumber { get; set; }
+
+         
+    }
+}
--- AddressBookDemo/AddressBookDemo/Program.cs
+++ AddressBookDemo/AddressBookDemo/Program.cs
@@ -7,6 +7,9 @@ namespace AddressBookDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To My Address Book Problem Solution By Amit Rana");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact("Amit", "Rana", "Uttarakhand", "Rishikesh", "UK", "amirana14325@gmail.com", 495698, 8979325434);
+            addressBook.ViewContact();
         }
     }
 }
