3931a1f00c2a22941332c80d05c98b42c9bb14ae
amit <[amirana14325@gmail.com]>
2021-04-02 01:54:12 +0530

    [Amit Rana]Added::mrthod for sorting Data by name

--- AddressBookDemo/AddressBookDemo/Program.cs
+++ AddressBookDemo/AddressBookDemo/Program.cs
@@ -32,7 +32,7 @@ namespace AddressBookDemo
             do
             {
                 Console.WriteLine($"Working On {bookName} AddressBook\n");
-                Console.WriteLine("Choose An Option \n1.Add New Contact \n2.Edit Existing Contact \n3.Delete A Contact \n4.View A Contact \n5.View All Contacts \n6.Add New AddressBook \n7.Switch AddressBook \n8.Search contact by city or state \n9.Count by City Or State \n0.exit Application\n");
+                Console.WriteLine("Choose An Option \n1.Add New Contact \n2.Edit Existing Contact \n3.Delete A Contact \n4.View A Contact \n5.View All Contacts \n6.Add New AddressBook \n7.Switch AddressBook \n8.Search contact by city or state \n9.Count by City Or State \n10.Sort Data By name\n0.exit Application\n");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -136,6 +136,9 @@ namespace AddressBookDemo
                         break;
                     case 9:
                         addressBook.DisplayCountByCityandState();
+                        break;
+                    case 10:
+                        addressBook.SortByName();
                         break;
                     default:
                         Console.WriteLine("Sorry!!! Invalid Entry");
